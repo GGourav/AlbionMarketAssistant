79a7b858bca6ec1e1dc34059e79f1c13931b7f0f
GGourav <67853722+GGourav@users.noreply.github.com>
2026-02-24 16:39:20 +0530

    Update StateMachine.kt

--- app/src/main/java/com/albion/marketassistant/statemachine/StateMachine.kt
+++ app/src/main/java/com/albion/marketassistant/statemachine/StateMachine.kt
@@ -9,7 +9,6 @@ import com.albion.marketassistant.media.ScreenCaptureManager
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlin.coroutines.coroutineContext
 
 class StateMachine(
     private val uiInteractor: UIInteractor,
