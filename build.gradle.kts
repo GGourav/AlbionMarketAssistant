f34892075a077fa5b1f85681e9e9773960a29879
GGourav <67853722+GGourav@users.noreply.github.com>
2026-02-25 15:28:02 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-// Path: build.gradle.kts (Top-level)
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
 }
