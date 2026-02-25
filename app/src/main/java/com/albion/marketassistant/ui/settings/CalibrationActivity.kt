efb98c1552f59f8d19629a22948c7dd406c7f972
GGourav <67853722+GGourav@users.noreply.github.com>
2026-02-25 23:47:05 +0530

    Update CalibrationActivity.kt

--- app/src/main/java/com/albion/marketassistant/ui/settings/CalibrationActivity.kt
+++ app/src/main/java/com/albion/marketassistant/ui/settings/CalibrationActivity.kt
@@ -1,6 +1,7 @@
 package com.albion.marketassistant.ui.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.albion.marketassistant.R
@@ -13,6 +14,10 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class CalibrationActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "CalibrationActivity"
+    }
 
     private val db by lazy { CalibrationDatabase.getInstance(this) }
     private var currentCalibration: CalibrationData? = null
@@ -79,7 +84,7 @@ class CalibrationActivity : AppCompatActivity() {
                     showTabContent(0)
                 }
             } catch (e: Exception) {
-                e.printStackTrace()
+                Log.e(TAG, "Error loading calibration", e)
                 withContext(Dispatchers.Main) {
                     populateFields(CalibrationData())
                     showToast("Error loading calibration: ${e.message}")
@@ -106,7 +111,6 @@ class CalibrationActivity : AppCompatActivity() {
         findViewById<EditText>(R.id.createOcrTop).setText(data.createMode.ocrRegionTop.toString())
         findViewById<EditText>(R.id.createOcrRight).setText(data.createMode.ocrRegionRight.toString())
         findViewById<EditText>(R.id.createOcrBottom).setText(data.createMode.ocrRegionBottom.toString())
-        // Default price and increment fields
         findViewById<EditText>(R.id.createDefaultPrice).setText(data.createMode.defaultBuyPrice.toString())
         findViewById<EditText>(R.id.createPriceIncrement).setText(data.createMode.priceIncrement.toString())
 
@@ -159,13 +163,13 @@ class CalibrationActivity : AppCompatActivity() {
         findViewById<CheckBox>(R.id.enableEndOfListDetection).isChecked = data.endOfList.enableEndOfListDetection
         findViewById<EditText>(R.id.identicalPageThreshold).setText(data.endOfList.identicalPageThreshold.toString())
         findViewById<EditText>(R.id.maxCyclesBeforeStop).setText(data.endOfList.maxCyclesBeforeStop.toString())
-        // NEW: End of List OCR region fields
+        
+        // End of List OCR region fields
         findViewById<EditText>(R.id.firstLineOcrLeft).setText(data.endOfList.firstLineOcrLeft.toString())
         findViewById<EditText>(R.id.firstLineOcrTop).setText(data.endOfList.firstLineOcrTop.toString())
         findViewById<EditText>(R.id.firstLineOcrRight).setText(data.endOfList.firstLineOcrRight.toString())
         findViewById<EditText>(R.id.firstLineOcrBottom).setText(data.endOfList.firstLineOcrBottom.toString())
 
-        // Default package name is now "com.albiononline"
         findViewById<CheckBox>(R.id.enableWindowVerification).isChecked = data.immersiveMode.enableWindowVerification
         findViewById<EditText>(R.id.gamePackageName).setText(data.immersiveMode.gamePackageName)
         findViewById<EditText>(R.id.windowLostThreshold).setText(data.immersiveMode.windowLostThreshold.toString())
@@ -191,117 +195,115 @@ class CalibrationActivity : AppCompatActivity() {
     private fun saveCalibration() {
         try {
             val createConfig = CreateModeConfig(
-                firstRowX = getInt(R.id.createFirstRowX, 100),
-                firstRowY = getInt(R.id.createFirstRowY, 300),
-                rowYOffset = getInt(R.id.createRowYOffset, 80),
-                maxRowsPerScreen = getInt(R.id.createMaxRows, 5),
-                priceInputX = getInt(R.id.createPriceX, 300),
-                priceInputY = getInt(R.id.createPriceY, 400),
-                createButtonX = getInt(R.id.createButtonX, 500),
-                createButtonY = getInt(R.id.createButtonY, 550),
-                confirmYesX = getInt(R.id.createConfirmYesX, 500),
-                confirmYesY = getInt(R.id.createConfirmYesY, 600),
-                closeButtonX = getInt(R.id.createCloseX, 1000),
-                closeButtonY = getInt(R.id.createCloseY, 200),
-                ocrRegionLeft = getInt(R.id.createOcrLeft, 600),
-                ocrRegionTop = getInt(R.id.createOcrTop, 200),
-                ocrRegionRight = getInt(R.id.createOcrRight, 1050),
-                ocrRegionBottom = getInt(R.id.createOcrBottom, 500),
-                // Default price and increment
-                defaultBuyPrice = getInt(R.id.createDefaultPrice, 10000),
-                priceIncrement = getInt(R.id.createPriceIncrement, 1)
+                firstRowX = getInt(R.id.createFirstRowX, 100, 0, 2000),
+                firstRowY = getInt(R.id.createFirstRowY, 300, 0, 4000),
+                rowYOffset = getInt(R.id.createRowYOffset, 80, 10, 500),
+                maxRowsPerScreen = getInt(R.id.createMaxRows, 5, 1, 20),
+                priceInputX = getInt(R.id.createPriceX, 300, 0, 2000),
+                priceInputY = getInt(R.id.createPriceY, 400, 0, 4000),
+                createButtonX = getInt(R.id.createButtonX, 500, 0, 2000),
+                createButtonY = getInt(R.id.createButtonY, 550, 0, 4000),
+                confirmYesX = getInt(R.id.createConfirmYesX, 500, 0, 2000),
+                confirmYesY = getInt(R.id.createConfirmYesY, 600, 0, 4000),
+                closeButtonX = getInt(R.id.createCloseX, 1000, 0, 2000),
+                closeButtonY = getInt(R.id.createCloseY, 200, 0, 4000),
+                ocrRegionLeft = getInt(R.id.createOcrLeft, 600, 0, 2000),
+                ocrRegionTop = getInt(R.id.createOcrTop, 200, 0, 4000),
+                ocrRegionRight = getInt(R.id.createOcrRight, 1050, 0, 2000),
+                ocrRegionBottom = getInt(R.id.createOcrBottom, 500, 0, 4000),
+                defaultBuyPrice = getInt(R.id.createDefaultPrice, 10000, 1, 10000000),
+                priceIncrement = getInt(R.id.createPriceIncrement, 1, 0, 10000)
             )
 
             val editConfig = EditModeConfig(
-                editButtonX = getInt(R.id.editButtonX, 950),
-                editButtonY = getInt(R.id.editButtonY, 300),
-                editButtonYOffset = getInt(R.id.editYOffset, 80),
-                priceInputX = getInt(R.id.editPriceX, 300),
-                priceInputY = getInt(R.id.editPriceY, 400),
-                updateButtonX = getInt(R.id.updateButtonX, 500),
-                updateButtonY = getInt(R.id.updateButtonY, 550),
-                confirmYesX = getInt(R.id.editConfirmYesX, 500),
-                confirmYesY = getInt(R.id.editConfirmYesY, 600),
-                closeButtonX = getInt(R.id.editCloseX, 1000),
-                closeButtonY = getInt(R.id.editCloseY, 200),
-                ocrRegionLeft = getInt(R.id.editOcrLeft, 600),
-                ocrRegionTop = getInt(R.id.editOcrTop, 200),
-                ocrRegionRight = getInt(R.id.editOcrRight, 1050),
-                ocrRegionBottom = getInt(R.id.editOcrBottom, 500)
+                editButtonX = getInt(R.id.editButtonX, 950, 0, 2000),
+                editButtonY = getInt(R.id.editButtonY, 300, 0, 4000),
+                editButtonYOffset = getInt(R.id.editYOffset, 80, 10, 500),
+                priceInputX = getInt(R.id.editPriceX, 300, 0, 2000),
+                priceInputY = getInt(R.id.editPriceY, 400, 0, 4000),
+                updateButtonX = getInt(R.id.updateButtonX, 500, 0, 2000),
+                updateButtonY = getInt(R.id.updateButtonY, 550, 0, 4000),
+                confirmYesX = getInt(R.id.editConfirmYesX, 500, 0, 2000),
+                confirmYesY = getInt(R.id.editConfirmYesY, 600, 0, 4000),
+                closeButtonX = getInt(R.id.editCloseX, 1000, 0, 2000),
+                closeButtonY = getInt(R.id.editCloseY, 200, 0, 4000),
+                ocrRegionLeft = getInt(R.id.editOcrLeft, 600, 0, 2000),
+                ocrRegionTop = getInt(R.id.editOcrTop, 200, 0, 4000),
+                ocrRegionRight = getInt(R.id.editOcrRight, 1050, 0, 2000),
+                ocrRegionBottom = getInt(R.id.editOcrBottom, 500, 0, 4000)
             )
 
             val globalConfig = GlobalSettings(
-                networkLagMultiplier = getFloat(R.id.networkLagMultiplier, 1.0f),
-                swipeStartX = getInt(R.id.swipeStartX, 500),
-                swipeStartY = getInt(R.id.swipeStartY, 600),
-                swipeEndX = getInt(R.id.swipeEndX, 500),
-                swipeEndY = getInt(R.id.swipeEndY, 300),
-                swipeDurationMs = getInt(R.id.swipeDuration, 300),
-                tapDurationMs = getLong(R.id.tapDuration, 150),
-                textInputDelayMs = getLong(R.id.textInputDelay, 200),
-                popupOpenWaitMs = getLong(R.id.popupOpenWait, 800),
-                popupCloseWaitMs = getLong(R.id.popupCloseWait, 600),
-                confirmationWaitMs = getLong(R.id.confirmationWait, 500),
-                cycleCooldownMs = getLong(R.id.cycleCooldown, 200)
+                networkLagMultiplier = getFloat(R.id.networkLagMultiplier, 1.0f, 0.1f, 10.0f),
+                swipeStartX = getInt(R.id.swipeStartX, 500, 0, 2000),
+                swipeStartY = getInt(R.id.swipeStartY, 600, 0, 4000),
+                swipeEndX = getInt(R.id.swipeEndX, 500, 0, 2000),
+                swipeEndY = getInt(R.id.swipeEndY, 300, 0, 4000),
+                swipeDurationMs = getInt(R.id.swipeDuration, 300, 50, 5000),
+                tapDurationMs = getLong(R.id.tapDuration, 150, 50, 1000),
+                textInputDelayMs = getLong(R.id.textInputDelay, 200, 0, 10000),
+                popupOpenWaitMs = getLong(R.id.popupOpenWait, 800, 100, 10000),
+                popupCloseWaitMs = getLong(R.id.popupCloseWait, 600, 100, 10000),
+                confirmationWaitMs = getLong(R.id.confirmationWait, 500, 100, 10000),
+                cycleCooldownMs = getLong(R.id.cycleCooldown, 200, 0, 10000)
             )
 
             val safetyConfig = SafetySettings(
                 enableOcrSanityCheck = findViewById<CheckBox>(R.id.enableOcrSanityCheck).isChecked,
-                maxPriceChangePercent = getFloat(R.id.maxPriceChangePercent, 0.2f),
-                maxPriceCap = getInt(R.id.maxPriceCap, 100000),
-                minPriceCap = getInt(R.id.minPriceCap, 1),
+                maxPriceChangePercent = getFloat(R.id.maxPriceChangePercent, 0.2f, 0.01f, 1.0f),
+                maxPriceCap = getInt(R.id.maxPriceCap, 100000, 1, 100000000),
+                minPriceCap = getInt(R.id.minPriceCap, 1, 1, 100000),
                 autoDismissErrors = findViewById<CheckBox>(R.id.autoDismissErrors).isChecked,
-                maxRetries = getInt(R.id.maxRetries, 3),
-                uiTimeoutMs = getLong(R.id.uiTimeout, 3000)
+                maxRetries = getInt(R.id.maxRetries, 3, 1, 20),
+                uiTimeoutMs = getLong(R.id.uiTimeout, 3000, 100, 60000)
             )
 
             val antiDetectionConfig = AntiDetectionSettings(
                 enableRandomization = findViewById<CheckBox>(R.id.enableRandomization).isChecked,
                 randomizeGesturePath = findViewById<CheckBox>(R.id.randomizeGesturePath).isChecked,
-                randomDelayRangeMs = getLong(R.id.randomDelayRange, 100),
-                pathRandomizationPixels = getInt(R.id.pathRandomizationPixels, 5)
+                randomDelayRangeMs = getLong(R.id.randomDelayRange, 100, 0, 5000),
+                pathRandomizationPixels = getInt(R.id.pathRandomizationPixels, 5, 0, 50)
             )
 
             val endOfListConfig = EndOfListSettings(
                 enableEndOfListDetection = findViewById<CheckBox>(R.id.enableEndOfListDetection).isChecked,
-                identicalPageThreshold = getInt(R.id.identicalPageThreshold, 3),
-                maxCyclesBeforeStop = getInt(R.id.maxCyclesBeforeStop, 500),
-                // NEW: OCR region fields
-                firstLineOcrLeft = getInt(R.id.firstLineOcrLeft, 100),
-                firstLineOcrTop = getInt(R.id.firstLineOcrTop, 300),
-                firstLineOcrRight = getInt(R.id.firstLineOcrRight, 900),
-                firstLineOcrBottom = getInt(R.id.firstLineOcrBottom, 350)
+                identicalPageThreshold = getInt(R.id.identicalPageThreshold, 3, 1, 20),
+                maxCyclesBeforeStop = getInt(R.id.maxCyclesBeforeStop, 500, 10, 10000),
+                firstLineOcrLeft = getInt(R.id.firstLineOcrLeft, 100, 0, 2000),
+                firstLineOcrTop = getInt(R.id.firstLineOcrTop, 300, 0, 4000),
+                firstLineOcrRight = getInt(R.id.firstLineOcrRight, 900, 0, 2000),
+                firstLineOcrBottom = getInt(R.id.firstLineOcrBottom, 350, 0, 4000)
             )
 
             val immersiveModeConfig = ImmersiveModeSettings(
                 enableWindowVerification = findViewById<CheckBox>(R.id.enableWindowVerification).isChecked,
-                gamePackageName = findViewById<EditText>(R.id.gamePackageName).text.toString(),
-                windowLostThreshold = getInt(R.id.windowLostThreshold, 2),
+                gamePackageName = findViewById<EditText>(R.id.gamePackageName).text.toString().trim(),
+                windowLostThreshold = getInt(R.id.windowLostThreshold, 2, 1, 10),
                 autoResumeOnReturn = findViewById<CheckBox>(R.id.autoResumeOnReturn).isChecked
             )
 
             val swipeOverlapConfig = SwipeOverlapSettings(
                 enableSwipeOverlap = findViewById<CheckBox>(R.id.enableSwipeOverlap).isChecked,
-                overlapRowCount = getInt(R.id.overlapRowCount, 1),
-                swipeSettleTimeMs = getLong(R.id.swipeSettleTime, 400)
+                overlapRowCount = getInt(R.id.overlapRowCount, 1, 0, 10),
+                swipeSettleTimeMs = getLong(R.id.swipeSettleTime, 400, 100, 5000)
             )
 
             val batteryConfig = BatterySettings(
                 enableBatteryOptimization = findViewById<CheckBox>(R.id.enableBatteryOptimization).isChecked,
-                pauseOnBatteryBelow = getInt(R.id.pauseOnBatteryBelow, 15)
+                pauseOnBatteryBelow = getInt(R.id.pauseOnBatteryBelow, 15, 5, 50)
             )
 
             val errorRecoveryConfig = ErrorRecoverySettings(
                 enableSmartRecovery = findViewById<CheckBox>(R.id.enableSmartRecovery).isChecked,
-                maxConsecutiveErrors = getInt(R.id.maxConsecutiveErrors, 5),
-                maxStateStuckTimeMs = getLong(R.id.maxStateStuckTime, 30000),
+                maxConsecutiveErrors = getInt(R.id.maxConsecutiveErrors, 5, 1, 50),
+                maxStateStuckTimeMs = getLong(R.id.maxStateStuckTime, 30000, 1000, 300000),
                 screenshotOnError = findViewById<CheckBox>(R.id.screenshotOnError).isChecked
             )
 
             val priceHistoryConfig = PriceHistorySettings(
                 enablePriceHistory = findViewById<CheckBox>(R.id.enablePriceHistory).isChecked,
                 enableAnomalyDetection = findViewById<CheckBox>(R.id.enableAnomalyDetection).isChecked,
-                maxHistoryEntries = getInt(R.id.maxHistoryEntries, 100)
+                maxHistoryEntries = getInt(R.id.maxHistoryEntries, 100, 10, 10000)
             )
 
             val updatedData = CalibrationData(
@@ -327,16 +329,57 @@ class CalibrationActivity : AppCompatActivity() {
                         showToast("Settings saved!")
                     }
                 } catch (e: Exception) {
-                    e.printStackTrace()
+                    Log.e(TAG, "Error saving calibration", e)
                     withContext(Dispatchers.Main) {
                         showToast("Error saving: ${e.message}")
                     }
                 }
             }
         } catch (e: Exception) {
+            Log.e(TAG, "Invalid input", e)
             showToast("Invalid input: ${e.message}")
         }
     }
 
     private fun resetToDefaults() {
-        val defaultData = CalibrationData
+        val defaultData = CalibrationData()
+        populateFields(defaultData)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                db.calibrationDao().insertCalibration(defaultData)
+                currentCalibration = defaultData
+                withContext(Dispatchers.Main) {
+                    showToast("Reset to defaults!")
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Error resetting", e)
+                withContext(Dispatchers.Main) {
+                    showToast("Error resetting: ${e.message}")
+                }
+            }
+        }
+    }
+
+    // Fixed: Added validation with min/max
+    private fun getInt(id: Int, default: Int, min: Int = Int.MIN_VALUE, max: Int = Int.MAX_VALUE): Int {
+        val text = findViewById<EditText>(id).text.toString()
+        val value = text.toIntOrNull() ?: default
+        return value.coerceIn(min, max)
+    }
+
+    private fun getLong(id: Int, default: Long, min: Long = Long.MIN_VALUE, max: Long = Long.MAX_VALUE): Long {
+        val text = findViewById<EditText>(id).text.toString()
+        val value = text.toLongOrNull() ?: default
+        return value.coerceIn(min, max)
+    }
+
+    private fun getFloat(id: Int, default: Float, min: Float = Float.MIN_VALUE, max: Float = Float.MAX_VALUE): Float {
+        val text = findViewById<EditText>(id).text.toString()
+        val value = text.toFloatOrNull() ?: default
+        return value.coerceIn(min, max)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
