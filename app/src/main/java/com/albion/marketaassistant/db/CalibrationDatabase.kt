1e49ad6fd5a03ebda7f3168de5da0a3d369c4882
GGourav <67853722+GGourav@users.noreply.github.com>
2026-02-28 20:52:51 +0530

    Create CalibrationDatabase.kt

---
+++ app/src/main/java/com/albion/marketaassistant/db/CalibrationDatabase.kt
@@ -0,0 +1,46 @@
+package com.albion.marketassistant.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.albion.marketassistant.data.CalibrationData
+import com.albion.marketassistant.data.SessionHistory
+import com.albion.marketassistant.data.ItemCache
+
+@Database(
+    entities = [
+        CalibrationData::class,
+        SessionHistory::class,
+        ItemCache::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class CalibrationDatabase : RoomDatabase() {
+    
+    abstract fun calibrationDao(): CalibrationDao
+    abstract fun sessionHistoryDao(): SessionHistoryDao
+    abstract fun itemCacheDao(): ItemCacheDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: CalibrationDatabase? = null
+        
+        fun getInstance(context: Context): CalibrationDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+        }
+        
+        private fun buildDatabase(context: Context): CalibrationDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                CalibrationDatabase::class.java,
+                "albion_market_assistant.db"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+}
